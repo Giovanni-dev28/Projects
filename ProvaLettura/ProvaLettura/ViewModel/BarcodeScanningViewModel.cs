8551a3f3c470bbb8d5a9d2b79797f8c0fa64b006
giovanni sciacca <giovanni.sciacca@freeloop.it>
2024-12-13 17:29:21 +0100

    Progetto lettura barcode

---
+++ ProvaLettura/ProvaLettura/ViewModel/BarcodeScanningViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ProvaLettura.ViewModel
+{
+    public class BarcodeScanningViewModel
+    {
+        public BarcodeScanningViewModel()
+        {
+            Barcode = string.Empty;
+        }
+
+        public string Barcode { get; set; }
+    }
+}
